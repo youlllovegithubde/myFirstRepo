383045e2f13708a3b27ff8c2c86edb1ddf5055f3
Tim <reich2011@hotmail.de>
2022-02-15 22:41:32 +0000

    v5

--- src/routes/v3.svelte
+++ src/routes/v3.svelte
@@ -1,3 +1,11 @@
+<script>
+	let mobileNav = { isOpen: false };
+
+	function toggle() {
+		mobileNav.isOpen = !mobileNav.isOpen;
+	}
+</script>
+
 <div class="flex flex-col min-h-screen">
     <header>
         <nav class="flex justify-between p-8">
@@ -7,8 +15,23 @@
                 <span class="text-xl p-4">Link 2</span>
                 <span class="text-xl p-4">Link 3</span>
             </div>
+            <div class="flex md:hidden" on:click={toggle}>
+                <button type="button" class="text-black dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                    <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                        <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                    </svg>
+                </button>
+            </div>
         </nav>
+        {#if mobileNav.isOpen}
+        <div class="flex flex-col lg:hidden text-gray-600 uppercase p-8">
+            <span class="text-xl p-4">Link 1</span>
+            <span class="text-xl p-4">Link 2</span>
+            <span class="text-xl p-4">Link 3</span>
+        </div>
+    {/if}
     </header>
+
     <main class="flex flex-grow">
         <div class="p-8 md:w-7/12">
             
