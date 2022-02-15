9db0a7eb7829ed09eb291f2021740f446e7e8ab7
Tim <reich2011@hotmail.de>
2022-02-15 18:43:19 +0000

    v2 with sidebar

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,5 +1,4 @@
-    <!--Footer-->
-    <footer class="bg-white">
+ <footer class="bg-white">
       <div class="container mx-auto px-8">
         <div class="w-full flex flex-col md:flex-row py-6">
           <div class="flex-1">
--- src/components/navigation.svelte
+++ src/components/navigation.svelte
@@ -7,7 +7,7 @@
 </script>
 
 
-<nav class="bg-white shadow dark:bg-gray-800">
+<nav class="fixed bg-white shadow dark:bg-gray-800 w-full">
 	<div class="container px-6 py-4 mx-auto lg:flex lg:justify-between lg:items-center">
 		<div class="lg:flex lg:items-center">
 			<div class="flex items-center justify-between">
